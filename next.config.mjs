a0feaaab2e12780fff2ce5bb9b2b248c6283d66f
Adugna Tadesse <adugtad@gmail.com>
2026-03-19 11:53:33 +0300

    latest

--- next.config.mjs
+++ next.config.mjs
@@ -8,9 +8,6 @@ try {
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   poweredByHeader: false,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
