2ecb4ef15a22035291b5fb1db1379776c0362f94
AbdisaR <108179677+AbdisaR@users.noreply.github.com>
2026-03-31 13:53:59 +0300

    google ad added

--- app/fifaworldcup/layout.tsx
+++ app/fifaworldcup/layout.tsx
@@ -1,3 +1,35 @@
+import type React from "react";
+import Script from "next/script";
+import { headers } from "next/headers";
+
+const GA_MEASUREMENT_ID = "G-9FE7R8MJ5H";
+
+export default async function FifaWorldCupLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const hdrs = await headers();
+  const nonce = hdrs.get("nonce") || "";
+
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="ga4-fifaworldcup-init" nonce={nonce} strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+      </Script>
+      {children}
+    </>
+  );
+}
 import type { ReactNode } from "react";
 
 import { FIFA_CUP_LOADER_SRC } from "./fifaLoaderAsset";
